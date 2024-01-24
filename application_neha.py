6504f89e012c97face8c32a74371686d5e11466e
princ <dsbyprince@gmail.com>
2024-01-24 10:48:30 +0530

    [1] False alarm

---
+++ application_neha.py
@@ -0,0 +1,35 @@
+from flask import Flask,request
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+@app.route("/train")
+def train():
+    df = pd.read_excel("path\False Alarm Cases.xlsx")
+    df.drop(["Case No.", "Unnamed: 8", "Unnamed: 9", "Unnamed: 10"], axis=1, inplace=True)
+    logr = LogisticRegression()
+    logr.fit(df.drop('Spuriosity Index(0/1)',axis=1),
+                                                 df['Spuriosity Index(0/1)'])
+    joblib.dump(logr,'train.pkl')
+
+    return "Model trained successfully..."
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    pkl_file = joblib.load('train.pkl')
+    data = request.get_json()
+    test_data = np.array([data["Ambient Temperature"], data["Calibration"] , data["Unwanted substance deposition"],
+    data["Humidity"] , data["H2S Content"], data["detected by"]]).reshape(1,6)
+
+    y_pred = pkl_file.predict(test_data)
+
+    if y_pred == 1:
+        return "False Alarm, No Danger"
+    else:
+        return "True Alarm, Danger "
+
+
+app.run(port=5000)
