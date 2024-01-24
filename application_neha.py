d4cfbe824413e7447d4b7919ef955a8de0fddf98
Nehapatil1505 <114587434+Nehapatil1505@users.noreply.github.com>
2024-01-24 11:03:44 +0530

    [1] False alar

--- application_neha.py
+++ application_neha.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/train")
 def train():
-    df = pd.read_excel("path\False Alarm Cases.xlsx")
+    df = pd.read_excel("G:\insta\False Alarm Cases.xlsx")
     df.drop(["Case No.", "Unnamed: 8", "Unnamed: 9", "Unnamed: 10"], axis=1, inplace=True)
     logr = LogisticRegression()
     logr.fit(df.drop('Spuriosity Index(0/1)',axis=1),
